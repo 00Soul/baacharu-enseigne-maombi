186c6b0f9c94d35512a367aadce26828e8303533
00Soul <lamar.s.taylor@live.com>
2015-05-16 18:47:17 -0400

    I think I'm settled on the app structure now

---
+++ winch/web/www/app/winch.js
@@ -0,0 +1,48 @@
+// Definition of the module for the Winch application, with routes 
+//    laid out for the views and controllers
+//
+(function() {
+    var app = angular.module('winch', [
+        'ngRoute'
+    ]);
+
+    app.controller('DefaultController', function($scope) {
+    });
+
+    app.controller('LoginController', function($scope) {
+    });
+
+    app.controller('RegistrationController', function($scope) {
+    });
+
+    app.controller('BoardController', function($scope) {
+    });
+
+    app.config(function($routeProvider, $locationProvider) {
+        var viewBase = '/views/'
+
+        $routeProvider
+        .when('/welcome', {
+            templateUrl: viewBase + 'welcome.html',
+            controller: 'DefaultController'
+        })
+        .when('/about', {
+            templateUrl: viewBase + 'about.html',
+            controller: 'DefaultController'
+        })
+        .when('/login', {
+            templateUrl: viewBase + 'login.html',
+            controller: 'LoginController'
+        })
+        .when('/register', {
+            templateUrl: viewBase + 'register.html',
+            controller: 'RegistrationController'
+        })
+        .when('/board/:boardId', {
+            templateUrl: viewBase + 'board.html',
+            controller: 'BoardController'
+        });
+
+        $locationProvider.hashPrefix('winch');
+    });
+}());
